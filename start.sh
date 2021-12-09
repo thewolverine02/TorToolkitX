33d0fe553defaac26d8e0036bf779e7faaa340b0
thewolverine02 <93479218+thewolverine02@users.noreply.github.com>
2021-12-09 15:37:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-python3 update.py && python3 -m bot
-
-aria2c --conf-path="aria2.conf" -D
-./alive.sh & gunicorn tortoolkit:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d & python3 -m tortoolkit
+python3 update.py && python3 -m bot 
